52b076229037100cee7ead112c8ec96d5d1419de
Susheet <59503121+Susheet@users.noreply.github.com>
2024-07-01 20:50:25 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version or newer JS
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3); console.log("Sk")  // not good readability
+
+console.log(3+3)
+console.log("Sk")
+
+
+let name = "sk"
+let age = 100
+let isLoggedIn = false
+
+// number => 2 to power 23 (range)
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => unique
+
+// object 
+
+console.log(typeof "sk");
+console.log(typeof age);
+
+console.log(typeof null);   // object
+console.log(typeof undefined); // undefined
